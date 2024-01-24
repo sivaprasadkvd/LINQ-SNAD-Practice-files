af0d0d41bcd63995f37f31f16ce801a72dec4f06
sivaprasadkvd <kvdsivaprasad2000@gmail.com>
2024-01-24 12:17:26 +0530

    kkvd

---
+++ Rough/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Rough
+{
+    public class Class1
+    {
+
+        public static void Main(String[] args)
+        {
+            var today = DateTime.Today;
+
+            var currentDay = today.Day;
+            var currentMonth = today.Month;
+            var currentYear = today.Year;
+
+            //Console.WriteLine(currentDay);
+
+
+            var dateString = currentYear.ToString() + "-" + currentMonth.ToString("D2") + "-" + currentDay.ToString("D2");
+            Console.WriteLine(dateString);
+            var dateOfJoining = DateTime.Parse(dateString);
+
+            Console.WriteLine(dateOfJoining);
+        }
+
+    }
+}
